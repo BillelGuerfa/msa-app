aa39e17032e9ca3c2e778f6e5fb805e2f23afa21
BillelGuerfa <cb_guerfa@esi.dz>
2016-03-26 20:57:13 +0100

    Add bindings to login component

--- app/users/components/login.component.ts
+++ app/users/components/login.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from 'angular2/core';
+import {ROUTER_DIRECTIVES } from "angular2/router";
+import {AuthService, Compte} from "../services/auth.service";
 declare var jQuery;
 @Component({
     selector: 'login',
     templateUrl: 'app/users/views/login.component.html',
-    
-   
+    providers: [AuthService],
+    directives: [ROUTER_DIRECTIVES] 
 })
 
 export class LoginComponent implements OnInit {
-    constructor() { }
+    compte: Compte;
+    constructor(private _authService: AuthService) { }
 
     ngOnInit() { 
+        
+    }
+    login(){
+        this._authService.login(this.compte).subscribe();
+    }
+    forget(){
         
     }
 
--- js/users/components/login.component.js
+++ js/users/components/login.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', "angular2/router", "../services/auth.service"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,25 +10,39 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, router_1, auth_service_1;
     var LoginComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (auth_service_1_1) {
+                auth_service_1 = auth_service_1_1;
             }],
         execute: function() {
             LoginComponent = (function () {
-                function LoginComponent() {
+                function LoginComponent(_authService) {
+                    this._authService = _authService;
                 }
                 LoginComponent.prototype.ngOnInit = function () {
+                };
+                LoginComponent.prototype.login = function () {
+                    this._authService.login(this.compte).subscribe();
+                };
+                LoginComponent.prototype.forget = function () {
                 };
                 LoginComponent = __decorate([
                     core_1.Component({
                         selector: 'login',
                         templateUrl: 'app/users/views/login.component.html',
+                        providers: [auth_service_1.AuthService],
+                        directives: [router_1.ROUTER_DIRECTIVES]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [auth_service_1.AuthService])
                 ], LoginComponent);
                 return LoginComponent;
             }());
